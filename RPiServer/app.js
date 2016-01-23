07d5edbf14ac1d6becb41451e0038a793e526bef
elklein96 <elklein96@gmail.com>
2016-01-23 17:18:08 -0500

    socket

--- RPiServer/app.js
+++ RPiServer/app.js
@@ -1,26 +1,41 @@
 var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var request = require('request');
+var http = require('http');
 var fs = require('fs');
 var path = require('path');
-
 var spawn = require('child_process').spawn;
+
 var proc;
 
-app.use('/', express.static(path.join(__dirname, 'stream')));
+var app = express();
+var server = http.createServer(app);
 
+app.use('/', express.static(path.join(__dirname, 'stream')));
+app.use(express.static(__dirname + '/public'));
+
+server.listen(process.env.PORT || 8888, function(){
+  console.log("Express server listening on port %d", this.address().port);
+});
 
 app.get('/', function(req, res) {
-	//res.sendFile(__dirname + '/index.html');
-	/*var io = require('socket.io').listen(server);
-	var sockets = {};*/
+	var io = require('socket.io').listen(server);
+	//var args = ["-rot", "180", "-o", "-", "-t", "0", "-hf", "-w", "800", "-h", "400", "-fps", "24", "|cvlc", "-vvv", "stream:///dev/stdin", "--sout", "'#standard{access=http,mux=ts,dst=:8160}'", ":demux=h264"];
+	//var stream = spawn('raspivid', args);
 
-	var args = ["-rot" "180" "-o" "-" "-t" "0" "-hf" "-w" "800" "-h" "400" "-fps" "24" "|cvlc" "-vvv" "stream:///dev/stdin" "--sout" "'#standard{access=http,mux=ts,dst=:8160}'" ":demux=h264"];
-	var stream = spawn('raspivid', args);
+	var stream = spawn('ping', ["google.com"]);
 
-	stream.stdout.on('data', function (data) {
-		console.log(data);
+
+	io.sockets.on('connection', function (socket) {
+		socket.on('disconnect', function() {
+			console.log("disconnected");
+		});
+
+		socket.on('start-stream', function() {
+			stream.stdout.on('64', function (data) {
+				socket.emit('data', data);
+				console.log(data);
+			});
+		});
 	});
 
 	/*var options = {
