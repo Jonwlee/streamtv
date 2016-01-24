6f78d64aa9c8819bac15c64a8c74b8a1742c6767
elklein96 <elklein96@gmail.com>
2016-01-24 02:28:06 -0500

    moment of truth

--- RPiServer/app.js
+++ RPiServer/app.js
@@ -72,41 +72,41 @@ function stopStreaming() {
 }*/
 
 function startStreaming(io) {
-    var blob;
-    var SOI = false;
-    var SOIChunk = false;
-    var SOIPos;
- 
-	var args = [ "-w", "160", "-h", "120", "-t", "999999999", "-tl", "750", "-o", "-" ];
-    proc = spawn("raspistill", args);
- 
-    proc.stdout.on("data", function(chunk) {
-        if(!SOI) {
-            for(var i = 0; i < chunk.length - 1; i++) {
-                if(chunk.readUInt8(i) == 0xFF && chunk.readUInt8(i + 1) == 0xD8 && !SOI) {
-                    SOI = true;
-                    SOIChunk = true;
-                    SOIPos = i;
-                }
-            }
-        }
- 
-        if(SOI) {
-            if(SOIChunk) {
-                blob = new Buffer(chunk.slice(SOIPos, chunk.length));
-                SOIChunk = false;
-            }
-            else
-                blob = Buffer.concat([blob, chunk]);
-        }
- 
-        if(chunk.readUInt8(chunk.length - 2) == 0xFF && chunk.readUInt8(chunk.length - 1) == 0xD9 && SOI) {
-            io.sockets.emit('liveStream', blob.toString("base64"));
-            SOI = false;
-        }
-    });
- 
-    proc.stderr.on("data", function(err) {
-        console.log(err.toString());
-    })
+	var blob;
+	var SOI = false;
+	var SOIChunk = false;
+	var SOIPos;
+
+	var args = [ "-w", "160", "-h", "120", "-t", "999999999", "-tl", "750", "-o", "-", "-q", "4"];
+	proc = spawn("raspistill", args);
+	
+	proc.stdout.on("data", function(chunk) {
+		if(!SOI) {
+			for(var i = 0; i < chunk.length - 1; i++) {
+				if(chunk.readUInt8(i) == 0xFF && chunk.readUInt8(i + 1) == 0xD8 && !SOI) {
+					SOI = true;
+					SOIChunk = true;
+					SOIPos = i;
+				}
+			}
+		}
+
+		if(SOI) {
+			if(SOIChunk) {
+				blob = new Buffer(chunk.slice(SOIPos, chunk.length));
+				SOIChunk = false;
+			}
+			else
+				blob = Buffer.concat([blob, chunk]);
+		}
+
+		if(chunk.readUInt8(chunk.length - 2) == 0xFF && chunk.readUInt8(chunk.length - 1) == 0xD9 && SOI) {
+			io.sockets.emit('liveStream', blob.toString("base64"));
+			SOI = false;
+		}
+	});
+
+	proc.stderr.on("data", function(err) {
+		console.log(err.toString());
+	})
 }
